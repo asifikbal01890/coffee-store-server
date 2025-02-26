c0401d54aef8f9fb19412aa5bdc80ae8fc614ae6
asifikbal01890 <asifikbal01890@gmail.com>
2025-02-26 17:52:28 +0600

    s

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -30,10 +30,28 @@ async function run() {
     const coffeeCollections = client.db("coffee_store").collection("coffee");
 
 
+    app.get('/coffee', async(req, res)=>{
+      const result = await coffeeCollections.find().toArray()
+      res.send(result)
+    })
+    app.get('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollections.findOne(query);
+      res.send(result)
+    })
+
     app.post('/coffee', async(req, res)=>{
         const data = req.body;
         const result = await coffeeCollections.insertOne(data);
         res.send(result)
+    })
+
+    app.delete('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollections.deleteOne(query)
+      res.send(result)
     })
 
 
